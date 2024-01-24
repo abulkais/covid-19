edf2d921fbc8268dfe737d8781e75fd62512888c
abulkais <abulkaish827@gmail.com>
2024-01-24 16:28:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [data, setuser] = useState([]);
+  const getuser = async () => {
+    try {
+    const res = await fetch('https://data.covid19india.org/data.json');
+    // console.log(res);
+    const data1 = await res.json();
+    // console.log(data1);
+    setuser(data1.statewise);
+    // console.log(data1.statewise)
+    }
+    catch(error){
+      alert("Warning! Please check the internet connection. This COVID API requires an active internet connection to function properly.");
+    }
+
+  }
+  useEffect(() => {
+    getuser();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+      <div className="container mt-5">
+        <h2 style={{ textAlign: 'center' }}>Indian States Covid-19 Dashboard</h2>
+        <div className="mt-5 table-responsive">
+          <table className="table table-striped table-hover table-bordered">
+            <thead className='table-secondary'>
+              <tr>
+                <th>State</th>
+                <th>Active</th>
+                <th>Confirmed</th>
+                <th>Deaths</th>
+                <th>DeltaConfirmed</th>
+                <th>DeltaDeaths</th>
+                <th>DeltaRecovered</th>
+                <th>LastUpdatedTime</th>
+                <th>Migratedother</th>
+                <th>Recovered</th>
+                <th>Statecode</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.map((val, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{val.state}</td>
+                      <td>{val.active}</td>
+                      <td>{val.confirmed}</td>
+                      <td>{val.deaths}</td>
+                      <td>{val.deltaconfirmed}</td>
+                      <td>{val.deltadeaths}</td>
+                      <td>{val.deltarecovered}</td>
+                      <td>{val.lastupdatedtime}</td>
+                      <td>{val.migratedother}</td>
+                      <td>{val.recovered}</td>
+                      <td>{val.statecode}</td>
+                    </tr>
+                  )
+                })
+              }
+
+            </tbody>
+          </table>
+          <h3 style={{textAlign:'center'}}> Create By Kaish</h3>
+        </div>
+      </div>
+    </>
   );
 }
 
